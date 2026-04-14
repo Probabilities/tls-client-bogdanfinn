5a53f939f1956cac88ab4087b0431cafafe64c4f
Socket <xmlrequest@proton.me>
2026-04-14 13:29:59 +0100

    added chrome 147

--- profiles/profiles.go
+++ profiles/profiles.go
@@ -32,7 +32,7 @@ var MappedTLSClients = map[string]ClientProfile{
 	"chrome_144_PSK":         Chrome_144_PSK,
 	"chrome_146":             Chrome_146,
 	"chrome_146_PSK":         Chrome_146_PSK,
-	"chrome_146_updated":     Chrome_146_Updated,
+	"chrome_147":             Chrome_147,
 	"brave_146":              Brave_146,
 	"brave_146_PSK":          Brave_146_PSK,
 	"safari_15_6_1":          Safari_15_6_1,
